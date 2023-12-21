1ab90cb4dc34e7a2fcbb9e13b086f05f672899dd
Luke P <lukewfigueroa@gmail.com>
2023-12-20 21:03:27 -0500

    Beginning of Project

---
+++ src/components/Who.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: #4799d3;
+`
+
+const Who = () => {
+  return (
+    <Section>Who</Section>
+  )
+}
+
+export default Who
